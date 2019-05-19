533889ba8f70fd73cd0519153d3503b79e2ef61b
nolongthing <18317773771@163.com>
2019-05-19 13:15:18 +0800

    5-19

--- personalBlog/index.js
+++ personalBlog/index.js
@@ -2,8 +2,13 @@ var configObj = require('./config');
 var express = require('express');
 var url = require('url');
 var loader = require('./loader.js');
+var svgCaptcha = require('svg-captcha')
 
 var app = new express();
+
+
+const cap = svgCaptcha.create();      //创建随机验证码对象
+
 
 app.use(express.static(configObj.web_path));
 
--- personalBlog/page/js/article.js
+++ personalBlog/page/js/article.js
@@ -1,11 +1,14 @@
 var vm = new Vue({
 	el:'#article',
+	data:{
+		article:{},
+	},
 	created() {
 		axios({
 			method: 'get',
 			url: '/getArticle'+location.search,
 		}).then(response => {
-			console.log(response);
+			this.article = response.data[0];
 		}).catch(function(error) {
 			console.log(error);
 		});
